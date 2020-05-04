e9455353ec09cd73a474c8b00eb776f69a3fbcf6
savayer <sava0094@gmail.com>
2020-05-04 17:15:57 +0800

    (add) more examples

--- js/async-await/index.js
+++ js/async-await/index.js
@@ -11,7 +11,7 @@ fetch_promise.onclick = () => {
             .then(() => fetch(url))
             .then(res => res.json())
     }
-    
+
     fetchData()
         .then(data => {
             console.log(data)
@@ -29,10 +29,70 @@ fetch_async.onclick = () => {
             const res = await fetch(url)
             const end = await res.json()
             console.log(end)
-        } catch(e) {
+        } catch (e) {
             console.error(e)
         }
     }
-    
+
     fetchAsync()
+}
+
+async_func.onclick = () => {
+    async function f() {
+        return 1;
+        // return Promise.resolve(1); the same
+    }
+
+    f().then(alert);
+}
+
+
+get_fullname_github_user.onclick = () => {
+
+    class HttpError extends Error {
+        constructor(response) {
+            super(`${response.status} for ${response.url}`);
+            this.name = 'HttpError';
+            this.response = response;
+        }
+    }
+
+    /* function loadJson(url) {
+        return fetch(url)
+            .then(response => {
+                if (response.status == 200) {
+                    return response.json();
+                } else {
+                    throw new HttpError(response);
+                }
+            })
+    } */
+
+    async function loadJson(url) {
+        const response = await fetch(url)
+        if (response.status == 200) {
+            return response.json();
+        }
+
+        throw new HttpError(response)
+    }
+
+    async function demoGithubUser() {
+        let name = prompt("Введите логин?", "acrossoffwest");
+
+        try {
+            const user = await loadJson(`https://api.github.com/users/${name}`)            
+            alert(`Полное имя: ${user.name}.`);
+        } catch (err) {
+            if (err instanceof HttpError && err.response.status == 404) {
+                alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
+                return demoGithubUser();
+            } else {
+                throw err;
+            }
+        }
+    }
+
+    demoGithubUser();
+
 }
